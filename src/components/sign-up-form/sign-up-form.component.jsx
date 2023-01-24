96f61890e085ca282a60cabb243c36e5f5b0e4cd
smail dahmani <smail.dahmani.98@gmail.com>
2023-01-24 23:04:44 +0100

    fix: cart dropdown scroll and button positionning

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -40,6 +40,7 @@ const SignUpFrom = () => {
         <div className="sign-up-container">
             <h2>Don't have an account?</h2>
             <span>Sign up with your email and password</span>
+
             <form onSubmit={handleSubmit}>
                 <FormInput
                     label="Display name"
